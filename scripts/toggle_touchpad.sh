c6854c3444bfb4ade3d817d027772145d78ea6ce
jiazhoulvke <jiazhoulvke@gmail.com>
2020-01-24 16:23:55 +0800

    调整音量

--- scripts/toggle_touchpad.sh
+++ scripts/toggle_touchpad.sh
@@ -3,6 +3,18 @@
 # 这里的"ELAN1010:00 04F3:3012 Touchpad"是我的笔记本上的触摸板型号
 TouchpadID=$(xinput list | grep "ELAN1010:00 04F3:3012 Touchpad" | cut -d"=" -f2 | awk '{print $1}')
 
+case $1 in
+	"enable")
+		xinput set-prop $TouchpadID "Device Enabled" 1
+		exit
+    ;;
+	"disable")
+		xinput set-prop $TouchpadID "Device Enabled" 0
+		exit
+    ;;
+esac
+
+
 status=$(xinput list-props $TouchpadID | grep "Device Enabled" | awk '{print $4}')
 
 if [ $status == "1" ];then
---
+++ scripts/toggle_volume.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cur_volume=$(amixer sget Master | tail -n 1 | awk '{print $4}' | tr -d "\[\]%")
+
+case $1 in
+	"up")
+		amixer sset Master 5%+
+		exit
+    ;;
+	"down")
+		amixer sset Master 5%-
+		exit
+    ;;
+esac
+
+if [ $cur_volume == "0" ];then
+	amixer sset Master 60%
+else
+	amixer sset Master 0%
+fi
