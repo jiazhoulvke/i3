502b08601270e7adb4044e9b6342ac808e3b6306
jiazhoulvke <jiazhoulvke@gmail.com>
2021-09-06 15:04:43 +0800

    小修改

--- scripts/background.sh
+++ scripts/background.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-background=$(find ~/.config/i3/assets/images/background/ -type f -name "*.jpg" -or -name "*.png" | grep -v default.jpg)
+background=$(find "$HOME/.config/i3/assets/images/background/" -type f -name "*.jpg" -or -name "*.png" | grep -v default.jpg)
 
-if [ -z $background ]; then
-	feh --bg-scale ~/.config/i3/assets/images/background/default.jpg
+if [ -z "$background" ]; then
+	feh --bg-scale "$HOME/.config/i3/assets/images/background/default.jpg"
 else
-	feh --bg-scale $background
+	feh --bg-scale "$background"
 fi
