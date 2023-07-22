26d461bf73e53a7f8da82d65f08e402f0d54de42
jiazhoulvke <jiazhoulvke@gmail.com>
2023-07-22 23:28:01 +0800

    config

--- scripts/toggle_touchpad.sh
+++ scripts/toggle_touchpad.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 这里的"ELAN1010:00 04F3:3012 Touchpad"是我的笔记本上的触摸板型号
-TouchpadID=$(xinput list | grep "ELAN1010:00 04F3:3012 Touchpad" | cut -d"=" -f2 | awk '{print $1}')
+TouchpadID=$(xinput list | grep -i " touchpad" | cut -d"=" -f2 | awk '{print $1}')
 
 case $1 in
 	"enable")
